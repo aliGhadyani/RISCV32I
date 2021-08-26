08900daff4749501e2dcb30bcefb66247f9fab1b
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-08-26 20:58:05 +0430

    new control modules added
    hazard unit
    forwarding unit
    control unit

---
+++ src/main/scala/RISCV32I/ForwardingUnit.scala
@@ -0,0 +1,10 @@
+package RISCV32I
+
+import chisel3._
+import chisel3.util._
+
+class ForwardingUnit extends Module {
+    val io = IO(new Bundle{
+
+    })
+}
--- src/main/scala/RISCV32I/RegFile.scala
+++ src/main/scala/RISCV32I/RegFile.scala
@@ -4,17 +4,17 @@ import chisel3._
 import chisel3.util._
 import scala.annotation.switch
 
-class RegFile(adressWidth: Int, blockWidth: Int) extends Module {
+class RegFile(blockNumber: Int, blockWidth: Int) extends Module {
     val io = IO(new Bundle{
         // write enable signal input
         val wr_en = Input(Bool())
 
         // read adresses input
-        val read_adr1 = Input(UInt(adressWidth.W))
-        val read_adr2 = Input(UInt(adressWidth.W))
+        val read_adr1 = Input(UInt(log2ciel(blockNumber).W))
+        val read_adr2 = Input(UInt(log2ciel(blockNumber).W))
 
         // write adress input
-        val write_adr = Input(UInt(adressWidth.W))
+        val write_adr = Input(UInt(log2ciel(blockNumber).W))
 
         // write data input
         val data_in = Input(UInt(blockWidth.W))
@@ -23,7 +23,7 @@ class RegFile(adressWidth: Int, blockWidth: Int) extends Module {
         val data_out1 = Output(UInt(blockWidth.W))
         val data_out2 = Output(UInt(blockWidth.W))
     })
-    val initVec = Seq.fill(2*adressWidth) {0.U(blockWidth.W)}
+    val initVec = Seq.fill(blockNumber) {0.U(blockWidth.W)}
     val regFile = RegInit(VecInit(initVec))
 
     io.data_out1 := regFile(io.read_adr1)
