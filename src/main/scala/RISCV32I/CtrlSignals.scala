755410e4199c8be1008cafc825e8662a1635d72c
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-08-31 22:15:27 +0430

    Update CtrlSignals.scala

--- src/main/scala/RISCV32I/CtrlSignals.scala
+++ src/main/scala/RISCV32I/CtrlSignals.scala
@@ -3,27 +3,44 @@ package RISCV32I
 import chisel3._
 import chisel3.util._
 
-object EXSig extends Bundle {
-    val slc_A = UInt(2.W)
-    val slc_B = UInt(2.W)
-    val slc_dst = UInt(2.W)
+class EXSig extends Bundle {
+    val slc_A   = UInt(2.W)
+    val slc_B   = UInt(2.W)
+    val alu_func= UInt(3.W)
+    val alu_f4  = Bool()
 }
 
-object MSig extends Bundle {
-    val wr_en = Bool()
-    val slc_dst = UInt(2.W)
+class MSig extends Bundle {
+    val wr_en   = Bool()
+    val slc_d   = UInt(2.W)
 }
 
-object WBSig extends Bundle {
-    val wr_en = Bool()
+class WBSig extends Bundle {
+    val wr_en   = Bool()
 }
 
-object Ctrl extends Bundle {
-    val jump    = Bool()
-    val slc_imm = UInt(3.W)
-    val f4      = UInt(2.W)
-    val M       = MSig
-    val EX      = EXSig
-    val WB      = WBSig
+object initSig {
+    val m_sig = new MSig() 
+    m_sig.wr_en     := false.B
+    m_sig.slc_dst   := 0.U
+
+    val wb_sig = new WBSig()
+    wb_sig.wr_en    := false.B
+
+    val ex_sig = new EXSig()
+    ex_sig.slc_A    := 0.U
+    ex_sig.slc_B    := 0.U
+    ex_sig.alu_func := 0.U
+    ex_sig.alu_f4   := false.B
+}
+
+class Ctrl extends Bundle {
+    val b_enable    = Bool()
+    val pc_rori     = Bool()
+    val jump        = Bool()
+    val slc_imm     = UInt(3.W)
+    val sig_M       = new MSig()
+    val sig_EX      = new EXSig()
+    val sig_WB      = new WBSig()
 }
         
