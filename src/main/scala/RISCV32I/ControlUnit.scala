bf0c7e4a0a0b5a4c95d9b130ff844c54d385415d
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-08-31 20:32:10 +0430

    Update ControlUnit.scala

--- src/main/scala/RISCV32I/ControlUnit.scala
+++ src/main/scala/RISCV32I/ControlUnit.scala
@@ -24,7 +24,19 @@ class CtrlUnit(instructionWidth: Int, functionWidth: Int) extends Module {
 
         }
         is("b1100011".U) {
+            io.out_ctrl.b_enable:= true.B
+            io.out_ctrl.func3   := io.in_inst(14, 12)
+            io.out_ctrl.slc_imm := 3.U
 
+            io.out_ctrl.sig_M.wr_en := false.B
+            io.out_ctrl.sig_M.sld_d := 0.U
+
+            io.out_ctrl.sig_WB.wr_en:= false.B
+
+            io.out_ctrl.sig_EX.slc_A:= 1.U
+            io.out_ctrl.sig_EX.slc_B:= 1.U
+            io.out_ctrl.sig_EX.alu_f4  := false.B
+            io.out_ctrl.sig_EX.alu_func:= "b000".U
         }
         is("b0000011".U) {
 
@@ -36,7 +48,7 @@ class CtrlUnit(instructionWidth: Int, functionWidth: Int) extends Module {
 
         }
         is("b0110011".U) {
-
+            io.out_ctrl.alu_func:= io.in_inst(14, 12)
         }
         is("b0001111".U) {
 
