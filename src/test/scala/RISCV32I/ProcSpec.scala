2d434f777e2616b24c1ba317bcf2e0d0a87a16c4
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-01 12:36:55 +0430

    Create ProcSpec.scala

---
+++ src/test/scala/RISCV32I/ProcSpec.scala
@@ -0,0 +1,20 @@
+package RISCV32I
+
+import chisel3._
+import chisel3.tester._
+import org.scalatest._
+import scala.util.Random
+
+class ProcSpec extends FreeSpec with ChiselScalatestTester {
+    var operand1 = Random.nextInt(200)
+    var operand2 = Random.nextInt(100)
+    "Proccessor functionality" in {
+        test(new Proccessor()) { dut =>
+            dut.io.in_inst_wr.poke(false.B)
+            dut.io.in_inst.poke("b00000000000000000000000000010011".U)
+
+            dut.clock.step(6)
+            dut.dp.rf.regFile(31.U).expect(7.U)
+        }
+    }
+}
