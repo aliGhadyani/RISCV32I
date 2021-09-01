95ebd67c4fac01d32573ada9754c56cc5a135f3f
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-01 07:53:38 +0430

    Update HzrdUnitSpec.scala

--- src/test/scala/RISCV32I/HzrdUnitSpec.scala
+++ src/test/scala/RISCV32I/HzrdUnitSpec.scala
@@ -9,19 +9,25 @@ import org.scalatest._
         test(new HazardUnit) { dut =>
            
             dut.io.in_branch.poke(true.B)
+            dut.io.in_jump.poke(false.B)
+            println("[test01] - only branch on")
+            println("[result] - stall=" + dut.io.out_stall.peek().toString + " kill=" + dut.io.out_kill.peek().toString)
             dut.io.out_stall.expect(true.B)
             dut.io.out_kill.expect(true.B)
 
             dut.io.in_branch.poke(false.B)
             dut.io.in_jump.poke(true.B)
+            println("[test02] - only jump on")
+            println("[result] - stall=" + dut.io.out_stall.peek().toString + " kill=" + dut.io.out_kill.peek().toString)
             dut.io.out_stall.expect(true.B)
-            dut.io.out_kill.expect(false.B)
+            dut.io.out_kill.expect(true.B)
 
             dut.io.in_branch.poke(false.B)
             dut.io.in_jump.poke(false.B)
+            println("[test03] - no one on")
+            println("[result] - stall=" + dut.io.out_stall.peek().toString + " kill=" + dut.io.out_kill.peek().toString)
             dut.io.out_stall.expect(false.B)
             dut.io.out_kill.expect(false.B)
-
         }
     }
 } 
