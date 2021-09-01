ef64862e5aa8ba94f97f7c7e528dbbf4e2e5d3cc
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-01 07:49:56 +0430

    PCR test modified
    module parameter removed
    test defined

--- src/test/scala/RISCV32I/PCSpec.scala
+++ src/test/scala/RISCV32I/PCSpec.scala
@@ -7,14 +7,36 @@ import scala.util.Random
 
  class PCSpec extends FreeSpec with ChiselScalatestTester {
     "Program Counter should work properly" in {
-        test(new PCR) { dut =>
+        test(new PCR()) { dut =>
             dut.io.in_pause.poke(false.B)
             dut.io.in_npc.poke(4.U)
-            dut.io.out_pc.poke(4.U)
+            println("[test01] - after initialization register most contains zero")
+            println("[result] - reg contains " + dut.io.out_pc.peek().toString())
+            dut.io.out_pc.expect(0.U)
+
+            dut.clock.step(1)
+
+            dut.io.in_pause.poke(false.B)
+            dut.io.in_npc.poke(121.U)
+            println("[test02] - in past clock 4 pushed in reg")
+            println("[result] - reg contains " + dut.io.out_pc.peek().toString())
+            dut.io.out_pc.expect(4.U)
+
+            dut.clock.step(1)
 
             dut.io.in_pause.poke(true.B)
-            dut.io.in_npc.poke(4.U)
-            dut.io.out_pc.poke(0.U)
+            dut.io.in_npc.poke(91.U)
+            println("[test03] - in past clock 121 pushed in reg")
+            println("[result] - reg contains " + dut.io.out_pc.peek().toString())
+            dut.io.out_pc.expect(121.U)
+
+            dut.clock.step(1)
+
+            dut.io.in_pause.poke(false.B)
+            dut.io.in_npc.poke(91.U)
+            println("[test04] - in past clock pause was on and 91 pushed")
+            println("[result] - reg contains " + dut.io.out_pc.peek().toString())
+            dut.io.out_pc.expect(121.U)
         }
     }
 } 
