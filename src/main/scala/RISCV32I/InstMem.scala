aac501a1cabc0ab083b83132659ab7d36be1c3d5
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-12 20:28:49 +0430

    Update InstMem.scala

--- src/main/scala/RISCV32I/InstMem.scala
+++ src/main/scala/RISCV32I/InstMem.scala
@@ -1,7 +1,6 @@
 package RISCV32I
 
 import chisel3._
-<<<<<<< HEAD
 import chisel3.util._
 import chisel3.util.experimental.loadMemoryFromFile
 
@@ -14,17 +13,3 @@ class InstMem extends Module {
     loadMemoryFromFile(mem, "C:/Users/Ghadyani/OneDrive/Desktop/im.hex")
     io.out_inst := mem(io.in_adr)
 }
-=======
-import chisel3.util.experimental.loadMemoryFromFile
-
-class InstMem extends Module{
-	val io = IO(new Bundle{
-		val in = Input(UInt(10.W))
-		val out = Output(UInt(32.W))
-	})
-
-	val mem = Mem(1024, UInt(32.W))
-	io.out := mem(io.in)
-	loadMemoryFromFile(mem, "C:/Users/Ghadyani/OneDrive/Desktop/im.bin")
-}
->>>>>>> 1080df5e3cd127f1c8145f94eb6a8fa57b0f586b
