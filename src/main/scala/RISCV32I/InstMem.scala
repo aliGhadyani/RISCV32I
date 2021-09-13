df0ba233f0651d658e46f4f969917d540a9222a5
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-13 22:58:01 +0430

    Update InstMem.scala

--- src/main/scala/RISCV32I/InstMem.scala
+++ src/main/scala/RISCV32I/InstMem.scala
@@ -6,10 +6,22 @@ import chisel3.util.experimental.loadMemoryFromFile
 
 class InstMem extends Module {
     val io  = IO(new Bundle{
-        val in_adr  = Input(UInt(12.W))
+        val in_adr  = Input(UInt(32.W))
         val out_inst= Output(UInt(32.W))
     })
-    val memory = Mem(1024, UInt(32.W))
-    io.out_inst := memory(io.in_adr >> 2)  
-    loadMemoryFromFile(memory, "C:/text.hex")
+    val init    = Seq(
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U,
+                "b0000_0000_0000_00000_000_00000_0010011".U
+            )
+    val memoery = RegInit(VecInit(init))
+    io.out_inst := memoery(io.in_adr)
+    /* val mem = Mem(1024, UInt(32.W))
+    loadMemoryFromFile(mem, "C:/Users/Ghadyani/OneDrive/Desktop/im.hex")
+    io.out_inst := mem(io.in_adr) */
 }
