8cf32b54f83b77c1dee17d8353da116c04f1cf02
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-09-29 19:31:49 +0330

    Create AXISpec.scala

---
+++ src/test/scala/RISCV32I/AXISpec.scala
@@ -0,0 +1,14 @@
+package RISCV32I
+
+import chisel3._
+import chisel3.tester._
+import org.scalatest._
+
+/* class AXISpec extends FreeSpec with ChiselScalatestTester {
+    "AXI functionality" in {
+        test(new AXIL_SLAVE()) { dut =>
+            
+
+        }
+    }
+} */
