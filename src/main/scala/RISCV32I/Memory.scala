fe8f1dec4158efdbe2ddebd09e1123fa438e354b
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-10-05 22:07:39 +0330

    Update Memory.scala

--- src/main/scala/RISCV32I/Memory.scala
+++ src/main/scala/RISCV32I/Memory.scala
@@ -4,111 +4,23 @@ import chisel3._
 import chisel3.util._ 
 import scala.annotation.switch
 
-class Memory(bytes: Int) extends Module {
+class Memory(LENGTH: Int, WORD_WIDTH: Int, STRB_WIDTH: Int) extends Module {
     val io = IO(new Bundle {
-        val in_func = Input(UInt(3.W))
-        val in_adr  = Input(UInt(32.W))
-        val in_w    = Input(UInt(32.W))
-        val in_d    = Input(UInt(32.W))
-        val in_wr_en= Input(Bool())
-        val out_w   = Output(UInt(32.W))
-        val out_d   = Output(UInt(32.W))
+        val in_wr_en    = Input(Bool())
+        val in_rd_en    = Input(Bool())
+        val in_strb     = Input(UInt(STRB_WIDTH.W))
+        val in_data     = Input(UInt(WORD_WIDTH.W))
+        val in_addr_wr  = Input(UInt(WORD_WIDTH.W))
+        val in_addr_rd  = Input(UInt(WORD_WIDTH.W))
+        val out_data    = Output(UInt(WORD_WIDTH.W))
     })
-    val initVec = Seq.fill(bytes) {0.U(8.W)}
-    val memFile = RegInit(VecInit(initVec))
-
-    io.out_w    := 0.U
-    io.out_d    := 0.U
-
-    switch(io.in_func){
-        is(0.U) {
-            io.out_w    := Cat(Fill(24, memFile(io.in_adr)(7)), memFile(io.in_adr))
-            io.out_d    := Fill(32, memFile(io.in_adr)(7))
-        }
-        is(1.U) {
-            io.out_w    := Cat(Fill(16, memFile(io.in_adr)(7)), memFile(io.in_adr), memFile(io.in_adr + 1.U))
-            io.out_d    := Fill(32, memFile(io.in_adr)(7))
-        }
-        is(2.U) {
-            io.out_w    := Cat(memFile(io.in_adr), memFile(io.in_adr + 1.U)
-                                , memFile(io.in_adr + 2.U), memFile(io.in_adr + 3.U))
-            io.out_d    := Fill(32, memFile(io.in_adr)(7))
-        }
-        is(3.U) {
-            io.out_w    := Cat(memFile(io.in_adr), memFile(io.in_adr + 1.U)
-                                 , memFile(io.in_adr + 2.U), memFile(io.in_adr + 3.U))
-            io.out_d    := Cat(memFile(io.in_adr + 4.U), memFile(io.in_adr + 5.U)
-                                , memFile(io.in_adr + 6.U), memFile(io.in_adr + 7.U))
-        }
-        is(4.U) {
-            io.out_w    := Cat(Fill(24, "b0".U), memFile(io.in_adr))
-            io.out_d    := Fill(32, "b0".U)
-        }
-        is(5.U) {
-            io.out_w    := Cat(Fill(16, "b0".U), memFile(io.in_adr), memFile(io.in_adr + 1.U))
-            io.out_d    := Fill(32, "b0".U)
-        }
-        is(6.U) {
-            io.out_w    := Cat(memFile(io.in_adr), memFile(io.in_adr + 1.U)
-                                , memFile(io.in_adr + 2.U), memFile(io.in_adr + 3.U))
-            io.out_d    := Fill(32, "b0".U)
-        }
-        is(7.U) {
-            io.out_w    := Cat(memFile(io.in_adr), memFile(io.in_adr + 1.U)
-                                , memFile(io.in_adr + 2.U), memFile(io.in_adr + 3.U))
-            io.out_d    := Cat(memFile(io.in_adr + 4.U), memFile(io.in_adr + 5.U)
-                                , memFile(io.in_adr + 6.U), memFile(io.in_adr + 7.U))
-        }
+    val in_data = Wire(Vec(STRB_WIDTH, UInt((WORD_WIDTH/STRB_WIDTH).W)))
+    for(i <- 0 until STRB_WIDTH) {
+        in_data(i)    := io.in_data(((WORD_WIDTH/STRB_WIDTH)*(i+1))-1, (WORD_WIDTH/STRB_WIDTH)*i)
     }
-
+    val memory  = SyncReadMem(LENGTH, Vec(STRB_WIDTH, UInt((WORD_WIDTH/STRB_WIDTH).W)))
     when(io.in_wr_en) {
-        switch(io.in_func){
-            is(0.U) {
-                memFile(io.in_adr)  := io.in_w(7, 0)
-            }
-            is(1.U) {
-                memFile(io.in_adr)  := io.in_w(15, 8)
-                memFile(io.in_adr + 1.U)  := io.in_w(7, 0)
-            }
-            is(2.U) {
-                memFile(io.in_adr)  := io.in_w(31, 25)
-                memFile(io.in_adr + 1.U)  := io.in_w(24, 17)
-                memFile(io.in_adr + 2.U)  := io.in_w(15, 8)
-                memFile(io.in_adr + 3.U)  := io.in_w(7, 0)
-            }
-            is(3.U) {
-                memFile(io.in_adr)  := io.in_d(31, 25)
-                memFile(io.in_adr + 1.U)  := io.in_d(24, 17)
-                memFile(io.in_adr + 2.U)  := io.in_d(15, 8)
-                memFile(io.in_adr + 3.U)  := io.in_d(7, 0)
-                memFile(io.in_adr + 4.U)  := io.in_w(31, 25)
-                memFile(io.in_adr + 5.U)  := io.in_w(24, 17)
-                memFile(io.in_adr + 6.U)  := io.in_w(15, 8)
-                memFile(io.in_adr + 7.U)  := io.in_w(7, 0)
-            }
-            is(4.U) {
-                memFile(io.in_adr)  := io.in_w(7, 0)
-            }
-            is(5.U) {
-                memFile(io.in_adr)  := io.in_w(15, 8)
-                memFile(io.in_adr + 1.U)  := io.in_w(7, 0)
-            }
-            is(6.U) {
-                memFile(io.in_adr)  := io.in_w(31, 25)
-                memFile(io.in_adr + 1.U)  := io.in_w(24, 17)
-                memFile(io.in_adr + 2.U)  := io.in_w(15, 8)
-                memFile(io.in_adr + 3.U)  := io.in_w(7, 0)
-            }
-            is(7.U) {
-                memFile(io.in_adr)  := io.in_d(31, 25)
-                memFile(io.in_adr + 1.U)  := io.in_d(24, 17)
-                memFile(io.in_adr + 2.U)  := io.in_d(15, 8)
-                memFile(io.in_adr + 3.U)  := io.in_d(7, 0)
-                memFile(io.in_adr + 4.U)  := io.in_w(31, 25)
-                memFile(io.in_adr + 5.U)  := io.in_w(24, 17)
-                memFile(io.in_adr + 6.U)  := io.in_w(15, 8)
-                memFile(io.in_adr + 7.U)  := io.in_w(7, 0)
-            }
-        }
+        memory.write(io.in_addr_wr(WORD_WIDTH-1, 2), in_data, io.in_strb)
     }
+    io.out_data     := memory.read(io.in_addr_rd, io.in_rd_en)
 }
