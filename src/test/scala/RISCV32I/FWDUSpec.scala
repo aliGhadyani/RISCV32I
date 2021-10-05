1b9f08ac682a10fb697051415ff004f09bd8e129
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2021-10-05 22:08:46 +0330

    Update FWDUSpec.scala

--- src/test/scala/RISCV32I/FWDUSpec.scala
+++ src/test/scala/RISCV32I/FWDUSpec.scala
@@ -5,7 +5,7 @@ import chisel3.tester._
 import org.scalatest._
 import scala.util.Random
 
- class FWDUSpec extends FreeSpec with ChiselScalatestTester {
+/*  class FWDUSpec extends FreeSpec with ChiselScalatestTester {
     "Forwarding Unit should provide proper functionality" in {
         test(new ForwardingUnit) { dut =>
             dut.io.in_M_reg_wr.poke(false.B)
@@ -54,4 +54,4 @@ import scala.util.Random
             dut.io.out_slc_B.expect(1.U)
         }
     }
-} 
+}  */
